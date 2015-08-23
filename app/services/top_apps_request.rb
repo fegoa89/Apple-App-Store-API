1cf593945a4a599f250e55750c8166eedbcb9192
Fernando <fgonzalezaguilera@gmail.com>
2015-08-23 02:32:42 +0200

    WIP - added new modules into the service folder for organize requests and responses

--- app/services/top_apps_request.rb
+++ app/services/top_apps_request.rb
@@ -1,20 +1,35 @@
 class TopAppsRequest
 
   include               ActiveModel::Model
-  attr_accessor         :category_id, :monetization
+  attr_accessor         :category_id, :monetization, :apple_store_request
   validates_presence_of :category_id, :monetization
 
   def perform_request
-    execute_api_call
+    get_top_apps_from_apple_store
+
+    if apple_store_response.valid?
+      top_charts_for_monetization_type
+    else
+      # Return error
+      {
+        error:    400,
+        message: "Bad Request"
+      }
+    end
   end
 
   private
 
-    def execute_api_call
-      RestClient.get request_configuration.apple_store_url, request_configuration.concatenate_for_apple_store, request_configuration.apple_store_header
+    def top_charts_for_monetization_type
+      apple_store_response.top_charts_for_monetization
     end
 
-    def request_configuration
-      @client_config ||= RequestConfiguration.new( { category_id: self.category_id, monetization: self.monetization } )
+    def apple_store_response
+      @apple_store_response ||= Response::AppleStoreResponse.new(apple_store_request, self.monetization)
     end
+
+    def get_top_apps_from_apple_store
+      @apple_store_request  ||= Client::AppleStoreApi.new(self.category_id, self.monetization).get_top_apps
+    end
+
 end
