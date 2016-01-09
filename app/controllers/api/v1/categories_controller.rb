259d30f37d028ae24f3eb09ec3e05b8efe218766
Fernando <fgonzalezaguilera@gmail.com>
2016-01-09 14:20:34 +0100

    formatted method content on categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,28 +3,15 @@ module Api
     class CategoriesController < ApplicationController
 
       def top_apps
-        if top_apps_request.valid?
-          @result = top_apps_request.perform_request
-        else
-          render_error(top_apps_request.errors.messages)
-        end
+        @result ||= top_apps_request.valid? ? top_apps_request.perform_request : render_error(top_apps_request.errors.messages)
       end
 
       def app_by_ranking_position
-        if app_ranking_position.valid?
-          @result = app_ranking_position.perform_request
-        else
-          render_error(app_ranking_position.errors.messages)
-        end
+        @result ||= app_ranking_position.valid? ? app_ranking_position.perform_request : render_error(app_ranking_position.errors.messages)
       end
 
-
       def publishers_ranking
-        if publishers_ranking_request.valid?
-          @result = publishers_ranking_request.perform_request
-        else
-          render_error(publishers_ranking_request.errors.messages)
-        end
+        @result ||= publishers_ranking_request.valid? ? publishers_ranking_request.perform_request : render_error(publishers_ranking_request.errors.messages)
       end
 
       private
